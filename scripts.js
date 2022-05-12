d8fc7c681a8f521d80cd164aa2bd2dca516a61e5
Emily R <emilyrib01@outlook.com>
2022-05-12 08:44:15 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,3 +4,30 @@ switchElement.addEventListener('click', () => {
     document.body.classList.toggle('dark')
 })
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the topnav
+var topnav = document.getElementById("topnav");
+
+// Get the offset position of the topnav
+var sticky = topnav.offsetTop;
+
+// Add the sticky class to the topnav when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    topnav.classList.add("sticky")
+  } else {
+    topnav.classList.remove("sticky");
+  }
+}
+
+$(function() {
+  $('.wrapper').click(function() {
+      $('.wrapper').each(function() {
+         $(this).css('z-index', 0); 
+      });
+      	$(this).css('z-index', 10); 
+      	$(this).toggleClass('open');    
+  }) 
+})
